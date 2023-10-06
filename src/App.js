0c08800265bc968f756b7573f2725234f02c60d2
Santy Cierco <santidisturbios@gmail.com>
2023-10-06 18:48:22 +0200

    he cambiado app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,8 @@
-import logo from './logo.svg';
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Effect from './useEffects/UseEfects';
-import FetchUseEffect from './useEffects/FetchUseEffect';
-import FetchUseEffect2 from './useEffects/FetchUseEffect2';
-import AddAlert from './useEffects/AddAlert';
-import Alert2 from './useEffects/Alert2';
-import ThreeCountersI from './useEffects/ThreeCountersI';
-import FormRefEffect from './useEffects/FormRefEffect';
-import Timeout from './useEffects/TimeOut';
-import Form from './useMemo/Form';
-import Memo from './useMemo/Memo';
-import AppTodoList from './useMemo/AppTodoList';
-import RenderChild from './useMemo/RenderChild';
-import Greeting from './useMemo/Greeting';
-import MemoTodo from './useMemo/MemoTodo';
-import Calculator from './useMemo/Calculator';
-import UseEfects from './useEffects/UseEfects';
-import UseEffectsCall from './useEffects/UseEffectsCall';
-import Counters from './useReducer/Counters'
-import TodoApp from './useReducer/TodoApp'
-import FormMemo from './useMemo/FormMemo';
 
-import CountersRef from './useRefs/CountersRef';
-import AddModal from './AddModal';
-import Stopwatch from './StopWatch';
-import ColorStyle from './ColorStyle';
-import TodoList from './useMemo/TodoList';
-import SimpleComponent from './useRefs/SimpleComponent';
-import Counter from './useState/Counter';
 
 function App() {
-
-
-
     return (
     <div>
         <Counter />
