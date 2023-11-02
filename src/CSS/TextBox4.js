0d4bcd4f21df97119fcbbafe0c0ccb1b2655137d
Santy Cierco <santidisturbios@gmail.com>
2023-11-02 15:13:38 +0100

    upload CSS

---
+++ src/CSS/TextBox4.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+function TextBox4() {
+  return <p style={{padding:'1rem', borderRadius:'6px', backgroundColor:'salmon', color:'white', fontSize:'20pxgit'}}>Cargamos la clase con los estilos implicitos</p>
+}
+
+export default TextBox4
